057aef70570503aa890e0552d20be3839d69fb58
Emily Tucker <emilytucker4@yahoo.com>
2019-09-22 16:17:44 -0400

    added better discriptions of what each require is for

--- liri.js
+++ liri.js
@@ -4,9 +4,13 @@ var spotify = new Spotify(keys.spotify);
 
 //getting the required info from each of these files so they can be read here
 var key = require("./keys.js");
+//read the spotify file in the node modules
 var Spotify = require("node-spotify-api");
+//read moment
 var moment = require("moment");
+//read axios
 var axios = require("axios");
+//read random.txt
 var fs = require("fs");
 
 // taking what the useer wants to like find a concert song movie or the random option
