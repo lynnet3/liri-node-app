3813a20a8d6329be6c58682181a6aaa4ae158982
Emily Tucker <emilytucker4@yahoo.com>
2019-09-22 16:05:54 -0400

    added the requires for each file so the liri.js can get to them and read them

--- liri.js
+++ liri.js
@@ -1,9 +1,17 @@
-// being able to read and write form these files
 require("dotenv").config();
 
-//var to get to my api key info
 var spotify = new Spotify(keys.spotify);
 
+//getting the required info from each of these files so they can be read here
+var key = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var axios = require("axios");
+var fs = require("fs");
+
+// taking what the useer wants to like 
+
+//functions to execute each action
 function concertInfo(){
 
 }
