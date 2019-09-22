04c18037e20ba02f733b885b7733ac4920667772
Emily Tucker <emilytucker4@yahoo.com>
2019-09-21 20:19:24 -0400

    added empty functions for each of the actions

--- liri.js
+++ liri.js
@@ -1,3 +1,21 @@
+// being able to read and write form these files
 require("dotenv").config();
 
+//var to get to my api key info
 var spotify = new Spotify(keys.spotify);
+
+function concertInfo(){
+
+}
+
+function songInfo(){
+
+}
+
+function movieInfo(){
+
+}
+
+function doThis(){
+    
+}
